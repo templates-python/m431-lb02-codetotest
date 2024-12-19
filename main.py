9720c972330221eb28c7a62c3b0e7247d19953e1
graphics80 <K.Ilkow@gmail.com>
2024-12-19 10:02:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,129 +1,80 @@
-from dataclasses import dataclass
-from datetime import datetime
-from typing import Optional
+from dataclasses import dataclass as __A
+from datetime import datetime as __D
+from typing import Optional as __O
 
+def __M():
+    __g = __m()
+    __z(__g)
+    __r(__g)
 
-def main():
-    """
-    main logic
-    :return:
-    """
-    accounting = init_accounts()
-    add_entries(accounting)
-    show_balance(accounting)
+def __m():
+    __f = dict()
+    for __p in ['Marco', 'Bernd', 'Adam']:
+        __f[__p] = list()
+    return __f
 
+def __z(__h):
+    __q = input('Kundenname > ')
+    __y = True
+    while __y:
+        __e = __X()
+        __h[__q].append(__e)
+        __l(__e)
+        __y = input('Weitere Bezüge (y/N) > ') == 'y'
 
-def init_accounts():
-    """
-    initializes the dictionary for the accounting
-    :return:
-    """
-    accounts = dict()
-    for person in ['Marco', 'Bernd', 'Adam']:
-        accounts[person] = list()
-    return accounts
+def __l(__E):
+    __E.__s = __w('Start des Ladevorgangs')
+    __E.__d = __w('Ende des Ladevorgangs')
+    __E.__u = __w('Freigabe der Säule')
+    __E.__n = __t('Strommenge in KWh >')
 
+def __r(__i):
+    for __c, __j in __i.items():
+        if len(__j) > 0:
+            __x = 0
+            print(f'Abrechnung für {__c}')
+            for __G in __j:
+                print(f'  + {__G.__s.strftime("%d.%m.%Y %H:%M")}: CHF {__G.__v:.2f}')
+                __x += __G.__v
+            print(f'Total: CHF {__x:.2f}')
 
-def add_entries(accounts):
-    """
-    adds the charging entries to the dictionary
-    :param accounts: dict with customers and entries
-    :return:
-    """
-    name = input('Kundenname > ')
-    more_entries = True
-    while more_entries:
-        entry = Entry()
-        accounts[name].append(entry)
-        read_entry(entry)
-
-        more_entries = input('Weitere Bezüge (y/N) > ') == 'y'
-
-
-def read_entry(entry):
-    """
-    prompts the user to enter the data for an entry
-    :return:
-    """
-    entry.start_time = read_datetime('Start des Ladevorgangs')
-    entry.end_time = read_datetime('Ende des Ladevorgangs')
-    entry.release_time = read_datetime('Freigabe der Säule')
-    entry.energy_amount = read_float('Strommenge in KWh >')
-
-
-def show_balance(accounting):
-    """
-    shows the balance for each customer
-    :param accounting: dict with customers and entries
-    :return:
-    """
-    for name, person in accounting.items():
-        if len(person) > 0:
-            total_cost = 0
-            print(f'Abrechnung für {name}')
-            for charge in person:
-                print(f'  + {charge.start_time.strftime("%d.%m.%Y %H:%M")}: CHF {charge.cost:.2f}')
-                total_cost += charge.cost
-            print(f'Total: CHF {total_cost:.2f}')
-
-
-def read_float(prompt):
-    """
-    prompts the user to enter a decimal number
-    :param prompt:
-    :return: the decimal number
-    """
-    number = None
-    while number is None:
+def __t(__o):
+    __B = None
+    while __B is None:
         try:
-            number = float(input(prompt + ' > '))
-            if number <= 0:
+            __B = float(input(__o + ' > '))
+            if __B <= 0:
                 print('Geben Sie eine positive Zahl ein')
-                number = None
+                __B = None
         except ValueError:
             print('Geben Sie eine positive Zahl ein')
-    return number
+    return __B
 
-
-def read_datetime(prompt):
-    """
-    prompts the user to enter a date/time
-    :param prompt:
-    :return: the datetime
-    """
-    timestamp = None
-    while timestamp is None:
-        user_input = input(prompt + '(dd.mm.jjjj hh:mm) > ')
+def __w(__W):
+    __Z = None
+    while __Z is None:
+        __Y = input(__W + '(dd.mm.jjjj hh:mm) > ')
         try:
-            timestamp = datetime.strptime(user_input, '%d.%m.%Y %H:%M')
+            __Z = __D.strptime(__Y, '%d.%m.%Y %H:%M')
         except ValueError:
             print('Geben Sie ein gültiges Datum/Uhrzeit ein')
-    return timestamp
+    return __Z
 
-
-@dataclass
-class Entry:
-    """
-    an accounting entry for the charging
-    """
-    start_time: Optional[datetime] = None
-    end_time: Optional[datetime] = None
-    release_time: Optional[datetime] = None
-    energy_amount: Optional[float] = None
+@__A
+class __X:
+    __s: __O[__D] = None
+    __d: __O[__D] = None
+    __u: __O[__D] = None
+    __n: __O[float] = None
 
     @property
-    def cost(self):
-        """
-        gets the total cost for the charging
-        :return:
-        """
-        energy_cost = self.energy_amount * 0.35
-        time_delta = self.release_time - self.end_time
-        duration = time_delta.total_seconds() / 60
-        if duration > 15:
-            energy_cost += (duration - 15) * 0.05
-        return round(energy_cost, 2)
-
+    def __v(self):
+        __K = self.__n * 0.35
+        __R = self.__u - self.__d
+        __L = __R.total_seconds() / 60
+        if __L > 15:
+            __K += (__L - 15) * 0.05
+        return round(__K, 2)
 
 if __name__ == '__main__':
-    main()
+    __M()
